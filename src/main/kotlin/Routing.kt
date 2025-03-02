ef0d65a50a6c12994d0e5c8d2e8c68bd242c1758
Philipp Scheede <github@p-scheede.de>
2025-03-02 19:43:50 +0100

    dummy endpoint with logs for auth subrequests

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,9 +1,25 @@
 package com.example
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    routing { get("/") { call.respondText("Hello incredibly beautiful World! Let's gooo!") } }
+    routing {
+        get("/") { call.respondText("Hello incredibly beautiful World! Let's gooo!") }
+
+        get("/auth") {
+            val specifiedHeaders =
+                    listOf("X-Original-URI", "X-Original-Remote-Addr", "X-Original-Host")
+
+            println("auth request")
+            specifiedHeaders.forEach { headerName ->
+                val headerValue = call.request.headers[headerName] ?: "Not Provided"
+                println("$headerName: $headerValue")
+            }
+
+            call.respond(HttpStatusCode.NoContent)
+        }
+    }
 }
